6a549595101382a9a90ff51c108335a6b0677cb6
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-14 12:00:28 +0600

    header area completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
+import profile from '../../assets/images/profile.png'
+
 const Header = () => {
     return (
         <header>
-            <h2>Knowledge Cafe</h2>
+            <div className='container mx-auto flex justify-between items-center py-8 border-b-2'>
+                <h2 className="text-4xl font-bold">Knowledge Cafe</h2>
+                <img src={profile} alt="profile photo" />
+            </div>
         </header>
     );
 };
