8d14c0f50d28fec62def333f8de05b5fae448a1e
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-14 16:35:57 +0600

    author's information added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,20 @@
 const Blog = ({blog}) => {
-    const {cover_photo, title} = blog;
+    const {cover_photo, author_img, author, posted_date, reading_time, title} = blog;
     return (
         <div>
-            <img src={cover_photo} alt={`image of ${title}`} />
+            <img className="rounded-xl" src={cover_photo} alt={`image of ${title}`} />
+            <div className="flex justify-between items-center mt-8">
+                <div className="flex items-center gap-5">
+                    <img className="w-16 h-16 object-cover rounded-full" src={author_img} alt="" />
+                    <div>
+                        <h4>{author}</h4>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>{reading_time} min read</p>
+                </div>
+            </div>
             <h2>{title}</h2>
         </div>
     );
