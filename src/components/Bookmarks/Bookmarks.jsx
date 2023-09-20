f3a855d9fa6de118a6c17d091ba3dd9292bceeb1
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-20 23:31:48 +0600

    bookmarked section complete

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -2,7 +2,43 @@ const Bookmarks = () => {
   return (
     <div className="mt-10">
       <div className="bg-violet-200 text-center py-5 rounded-lg border border-violet-500">
-        <h3 className="text-2xl font-bold text-violet-500">Spent time on read : 177 min</h3>
+        <h3 className="text-2xl font-bold text-violet-500">
+          Spent time on read : 177 min
+        </h3>
+      </div>
+
+      <div className="bg-gray-100 rounded-lg mt-6 p-7">
+        <h3 className="text-2xl font-bold mb-4">Bookmarked Blogs: 8</h3>
+        
+        <div className="bg-white p-5 rounded-lg mb-6">
+          <h4 className="text-lg font-semibold">
+            Master Microsoft Power Platform and Become an In-Demand!
+          </h4>
+        </div>
+        
+        <div className="bg-white p-5 rounded-lg mb-6">
+          <h4 className="text-lg font-semibold">
+            Master Microsoft Power Platform and Become an In-Demand!
+          </h4>
+        </div>
+        
+        <div className="bg-white p-5 rounded-lg mb-6">
+          <h4 className="text-lg font-semibold">
+            Master Microsoft Power Platform and Become an In-Demand!
+          </h4>
+        </div>
+        
+        <div className="bg-white p-5 rounded-lg mb-6">
+          <h4 className="text-lg font-semibold">
+            Master Microsoft Power Platform and Become an In-Demand!
+          </h4>
+        </div>
+        
+        <div className="bg-white p-5 rounded-lg mb-6">
+          <h4 className="text-lg font-semibold">
+            Master Microsoft Power Platform and Become an In-Demand!
+          </h4>
+        </div>
       </div>
     </div>
   );
