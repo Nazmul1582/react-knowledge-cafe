03e858a18b68e983a30fbbea2a0bc09afaa2aa2c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-20 23:23:17 +0600

    spent time of bookmark section added

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,11 @@
 const Bookmarks = () => {
-    return (
-        <div>
-            <h3>Bookmarks</h3>
-        </div>
-    );
+  return (
+    <div className="mt-10">
+      <div className="bg-violet-200 text-center py-5 rounded-lg border border-violet-500">
+        <h3 className="text-2xl font-bold text-violet-500">Spent time on read : 177 min</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Bookmarks;
