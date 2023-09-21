cba9134c7ee11512bec4e36f067f3d581593971e
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-21 11:18:27 +0600

    changed bookmark style and deployed surge with added CNAME

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -2,20 +2,20 @@ import PropTypes from 'prop-types'
 
 const Bookmarks = ({ bookmarks, readingTime }) => {
   return (
-    <div className="mt-10">
+    <div className="mt-10 flex flex-col">
       <div className="bg-violet-200 text-center py-5 px-3 rounded-lg border border-violet-500">
         <h3 className="text-2xl font-bold text-violet-500">
           Spent time on read : {readingTime} min
         </h3>
       </div>
 
-      <div className="bg-gray-100 rounded-lg mt-6 p-7">
+      <div className="bg-gray-100 rounded-lg mt-6 p-7 flex-grow">
         <h3 className="text-2xl font-bold mb-4">
           Bookmarked Blogs: {bookmarks.length}
         </h3>
 
-        {bookmarks.map((bookmark, index) => (
-          <div key={index} className="bg-white p-5 rounded-lg mb-6 last:mb-0">
+        {bookmarks.map((bookmark) => (
+          <div key={bookmark.id} className="bg-white p-5 rounded-lg mb-6 last:mb-0">
             <h4 className="text-lg font-semibold">{bookmark.title}</h4>
           </div>
         ))}
