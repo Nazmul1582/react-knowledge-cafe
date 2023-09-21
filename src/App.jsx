e254acff71901111bc8d417c4ed086724a207852
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-21 09:16:49 +0600

    show warning if blog already remianed in bookmark

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,27 @@ import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [bookmarks, setBookmarks] = useState([]);
+  const [warning, setWarning] = useState("");
 
-  const handleBookmark = (title) => setBookmarks([...bookmarks, title]);
+  const handleBookmark = (title) => {
+    if (bookmarks.includes(title)) {
+      setWarning(toast.warn("Already added in bookmark!"));
+      return;
+    }
+    setBookmarks([...bookmarks, title]);
+  };
 
+  console.log(warning);
   return (
     <>
       <Header />
       <main className="container mx-auto">
+        <ToastContainer pauseOnHover={false} />
         <section className="grid grid-cols-[2fr_1fr] gap-6">
           <Blogs handleBookmark={handleBookmark} />
           <Bookmarks bookmarks={bookmarks} />
